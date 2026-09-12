dfc48f9c48551b914675264128829c3e48e2719f
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-09-12 17:11:53 +0200

    backend/config.js

---
+++ otp-security-system/backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MAX_REQUESTS_PER_HOUR: 3,
+  OTP_EXPIRY: 30, // seconds
+  RESEND_WINDOW: 5, // minutes
+  MAX_RESENDS: 3,
+  EMAIL_USER: "your_email@gmail.com",
+  EMAIL_PASS: "your_app_password"
+};
